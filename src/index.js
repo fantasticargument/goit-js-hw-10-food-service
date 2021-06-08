fa87aa7a1d339c2117a22888486bda839486f7f2
Petryk Roman <romic.argument@gmail.com>
2021-06-08 16:00:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import cardMenu from './templates/menu-card.hbs';
 import dishes from './menu.json';
 
-const menuConteiner = document.querySelector('.js-menu')
+const rfs = {
+menuConteiner : document.querySelector('.js-menu'),
+chekBocsTurner: document.querySelector('#theme-switch-toggle'),
+bodyProfile: document.querySelector('body')
+}
 const menuMarkup = createMenu(dishes);
+rfs.menuConteiner.insertAdjacentHTML('beforeend', menuMarkup);
 
-menuConteiner.insertAdjacentHTML('beforeend', menuMarkup)
+rfs.chekBocsTurner.addEventListener('click', changeProfile)
 
 function createMenu(dishes) {
     return dishes.map(cardMenu).join(' ')
+}
+
+function changeProfile() {
+    rfs.bodyProfile.classList.add('dark-theme')
 }
