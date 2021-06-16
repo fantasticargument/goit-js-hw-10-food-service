9ce6ca7c2781ad0bdd84cd6e28e8b65cf7279d89
Petryk Roman <romic.argument@gmail.com>
2021-06-16 20:01:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,33 @@ menuConteiner.insertAdjacentHTML('beforeend', menuMarkup);
 function createMenu(dishes) {
     return dishes.map(cardMenu).join(' ')
 }
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const rfs = {
+  chekBocsTurner: document.querySelector('#theme-switch-toggle'),
+  bodyTheme: document.querySelector('body'),
+};
+
+const statusCheckBox = localStorage.getItem('chekBocs');
+
+if (statusCheckBox === 'on') {
+  rfs.chekBocsTurner.checked = true;
+  rfs.bodyTheme.classList.add(Theme.DARK);
+} else {
+  rfs.bodyTheme.classList.add(Theme.LIGHT);
+}
+
+rfs.chekBocsTurner.addEventListener('click', addVAlueLocalStorage);
+
+function addVAlueLocalStorage() {
+  rfs.chekBocsTurner.checked?
+    localStorage.setItem('chekBocs', 'on'):
+    localStorage.setItem('chekBocs', 'off');
+  
+  rfs.bodyTheme.classList.toggle(Theme.LIGHT);
+  rfs.bodyTheme.classList.toggle(Theme.DARK);
+}
